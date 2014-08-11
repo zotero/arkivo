7e5b861e6f0cd1e5f5317128970768adf1880ad6
Sylvester Keil <sylvester@keil.or.at>
2014-08-11 14:01:14 +0200

    add subscription tests

--- test/subscription.js
+++ test/subscription.js
@@ -1,8 +1,11 @@
+var sinon = require('sinon');
 var chai = require('chai');
 var expect = chai.expect;
 
 chai.use(require('chai-as-promised'));
+chai.use(require('sinon-chai'));
 
+var B = require('bluebird');
 var Subscription = require('../lib/subscription');
 
 describe('Subscription', function () {
@@ -74,6 +77,104 @@ describe('Subscription', function () {
 
       expect(s.pathname).to.equal('tra/la/la');
       expect(s.params).to.have.property('debug', 'false');
+    });
+  });
+
+  describe('.load', function () {
+    afterEach(function () {
+      Subscription.db.hgetall.restore();
+    });
+
+    describe('for existing subscriptions', function () {
+      beforeEach(function () {
+        sinon.stub(Subscription.db, 'hgetall', function () {
+          return B.fulfilled({ id: 'foo', bar: 'baz' });
+        });
+      });
+
+      it('returns a promise for the subscription', function () {
+        return expect(Subscription.load('foo'))
+          .to.be.instanceOf(B)
+          .and.eventually.be.fulfilled
+          .and.instanceOf(Subscription)
+          .and.have.property('bar', 'baz');
+      });
+    });
+
+    describe('for non-existing subscriptions', function () {
+      beforeEach(function () {
+        sinon.stub(Subscription.db, 'hgetall', function () {
+          return B.fullfilled({});
+        });
+      });
+
+      it('fails if the subscription does not exist', function () {
+        return expect(Subscription.load('foo')).to.eventually.be.rejected;
+      });
+    });
+  });
+
+  describe('#save', function () {
+    function chainspy() {
+      return sinon.spy(function () { return this; });
+    }
+
+    beforeEach(function () {
+      sinon.stub(Subscription.db, 'transaction', function () {
+        return {
+          sadd: chainspy(),
+          hmset: chainspy(),
+          commit: B.fulfilled.bind(B)
+        };
+      });
+    });
+
+    afterEach(function () {
+      Subscription.db.transaction.restore();
+    });
+
+    describe('for new subscriptions', function () {
+      beforeEach(function () {
+        sinon.stub(Subscription, 'exists', function () {
+          return B.fulfilled(false);
+        });
+      });
+
+      afterEach(function () { Subscription.exists.restore(); });
+
+      it('returns a promise for the saved subscription', function () {
+        var s = new Subscription({ id: 'bar', url: 'foo' });
+
+        return expect(s.save())
+          .to.eventually.be.fulfilled
+          .and.equal(s)
+          .and.have.property('url', 'foo');
+      });
+
+      it('generates a new id if necessary', function () {
+        var s = new Subscription({ url: 'foo' });
+
+        return expect(s.save())
+          .to.eventually.be.fulfilled
+          .and.equal(s)
+          .and.have.property('id').and.have.length(10);
+      });
+    });
+
+    describe('for existing subscriptions', function () {
+      it('returns a promise for the saved subscription', function () {
+      });
+
+      it('does not alter the id', function () {
+      });
+    });
+  });
+
+  describe('#identify', function () {
+    it('returns a promise for the subscription with an id', function () {
+    });
+
+    it('sets a unique id', function () {
     });
   });
 });

37d080eadac3e32e3ff644151a54e866ebdf29cb
Sylvester Keil <sylvester@keil.or.at>
2014-09-19 20:26:14 +0200

    add tabtab (wip)

--- bin/arkivo.js
+++ bin/arkivo.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var tabtab  = require('tabtab');
 
 program
   .version(require('../package.json').version)
@@ -9,6 +10,27 @@ program
   .command('plugins', 'Manage Arkivo plugins')
 
   .command('subscriptions', 'Manage Zotero URL subscriptions')
-  .command('sync', 'Synchronize one or more subscriptions')
+  .command('sync', 'Synchronize one or more subscriptions');
 
-  .parse(process.argv);
+
+// --- Tab-Completion ---
+
+if (process.argv[2] === 'completion') {
+  return tabtab.complete('arkivo', function (error, data) {
+    if (error || !data) return undefined;
+
+    function to_long(o)  { return o.long;  }
+    function to_short(o) { return o.short; }
+    function to_name(o)  { return o._name; }
+
+    if ((/^--/).test(data.last))
+      return tabtab.log(program.options.map(to_long), data, '--');
+
+    if ((/^-/).test(data.last))
+      return tabtab.log(program.options.map(to_short), data, '-');
+
+    tabtab.log(program.commands.map(to_name), data);
+  });
+}
+
+program.parse(process.argv);

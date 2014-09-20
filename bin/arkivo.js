e1f34067bed32cc5bb846458261a215ddce6aa9e
Sylvester Keil <sylvester@keil.or.at>
2014-09-20 23:17:13 +0200

    add up command to main executable

--- bin/arkivo.js
+++ bin/arkivo.js
@@ -1,17 +1,72 @@
 #!/usr/bin/env node
+'use strict';
 
+// --- Dependencies ---
+
+require('gnode');
+
+var debug    = require('debug')('arkivo');
+//var B       = require('B');
 var program = require('commander');
 var tabtab  = require('tabtab');
 
+var arkivo  = require('..');
+
+var controller = arkivo.controller;
+
 program
-  .version(require('../package.json').version)
+  .version(arkivo.version)
 
-  .command('start', 'Start the Arkivo service')
+  .option('-r, --redis <host:port>', 'configure the Redis connection', redis);
+
+
+// --- Command ---
+
+program
+  .command('up')
+  .description('start the Arkivo service')
+
+  .action(function up() {
+
+    controller.start();
+
+    process.once('SIGTERM', function () {
+      debug('sigterm received: shutting down...');
+
+      controller
+        .stop()
+        .then(shutdown)
+
+        .catch(function (error) {
+          debug('failed to shut down gracefully: %s', error.message);
+          debug(error.stack);
+
+          process.exit(1);
+        });
+    });
+  });
+
+
+program
   .command('plugins', 'Manage Arkivo plugins')
+  .command('subscriptions', 'Manage Zotero URL subscriptions');
 
-  .command('subscriptions', 'Manage Zotero URL subscriptions')
-  .command('sync', 'Synchronize one or more subscriptions');
 
+// --- Helpers ---
+
+function shutdown() {
+  arkivo.db.reset();
+  process.stdin.destroy();
+}
+
+function redis(input) {
+  var cfg = input.split(':');
+
+  if (cfg[0]) arkivo.config.redis.host = cfg[0];
+  if (cfg[1]) arkivo.config.redis.port = cfg[1];
+
+  return cfg;
+}
 
 // --- Tab-Completion ---
 

4f2769f5b96c7150bc8556591a1c677d788e06dc
Sylvester Keil <sylvester@keil.or.at>
2014-09-23 16:02:47 +0200

    update subscriptions cli
    all commands use the finder now

--- bin/arkivo-subscriptions.js
+++ bin/arkivo-subscriptions.js
@@ -3,76 +3,30 @@
 
 require('gnode');
 
+
+// --- Dependencies ---
+
 var B       = require('bluebird');
 var program = require('commander');
-var user    = require('co-prompt');
+var cp      = require('co-prompt');
 
 var arkivo = require('..');
 var common = require('../lib/common');
 
 var Subscription = arkivo.Subscription;
+var capitalize = common.capitalize;
 
 
-// --- Helper Functions ---
-
-function shutdown() {
-  Subscription.db.reset();
-  process.stdin.destroy();
-}
-
-function plugin(string, list) {
-  return list.concat(string.split(','));
-}
-
-function confirm(question) {
-  return new B(function (resolve, reject) {
-    user.confirm(question)(function (error, ok) {
-      if (error) return reject(error);
-      resolve(ok);
-    });
-  });
-}
-
-function confirmable(method, ids, action) {
-  ids = ids.split(',');
-
-  var question = common.capitalize(method) +
-    ' ' + ids.length + ' subscription(s). Proceed? ';
-
-  return confirm(question)
-    .then(function (ok) {
-      return ok ? load(ids).each(action) : [];
-    })
-
-    .catch(function (error) {
-      console.log( 'Failed to %s subscription(s): %s', method, error.message);
-      console.error(error.stack);
-    });
-}
-
-function load(ids) {
-  return B.map(ids, function (id) {
-    return Subscription.load(id);
-  });
-}
-
-function redis(input) {
-  var cfg = input.split(':');
-
-  if (cfg[0]) arkivo.config.redis.host = cfg[0];
-  if (cfg[1]) arkivo.config.redis.port = cfg[1];
-
-  return cfg;
-}
-
-
-// --- Commands ---
+// --- Global Options ---
 
 program
   .version(arkivo.version)
 
   .option('-k, --keys', 'show Zotero API keys in output')
   .option('-r, --redis <host:port>', 'configure the Redis connection', redis);
+
+
+// --- Commands ---
 
 program
   .command('list')
@@ -86,24 +40,17 @@ program
 
     Subscription
       .all()
-
-      .tap(function (s) {
-        console.log('%d subscription(s) found.', s.length);
-      })
-
+      .tap(num('found'))
       .each(print)
 
-      .catch(function (error) {
-        console.log('Failed to list subscriptions: %s', error.message);
-        console.error(error.stack);
-      })
+      .catch(backtrace('Failed to list subscriptions'))
 
       .finally(shutdown);
   });
 
 
 program
-  .command('show <subscriptions>')
+  .command('show [subscriptions]')
   .description('Print details of given subscription(s)')
 
   .action(function show(ids) {
@@ -120,10 +67,14 @@ program
     }
 
     function to_name(o) { return o.name; }
+    function separate() { console.log(''); }
 
-    load(ids.split(','))
+    Subscription
+      .find(ids)
+      .tap(num('found'))
+
       .each(function (s, idx) {
-        if (idx) console.log('');
+        if (idx) separate();
 
         print('Subscription', s.id);
         print('Zotero URL', s.url);
@@ -137,10 +88,7 @@ program
         print('Last updated at', s.timestamp);
       })
 
-      .catch(function (error) {
-        console.log('Failed to show subscription(s): %s', error.message);
-        console.error(error.stack);
-      })
+      .catch(backtrace('Failed to show subscription(s)'))
 
       .finally(shutdown);
   });
@@ -148,20 +96,22 @@ program
 
 
 program
-  .command('remove <subscriptions>')
+  .command('remove [subscriptions]')
   .description('Remove the given subscription(s)')
 
   .action(function remove(ids) {
+    Subscription
+      .find(ids)
 
-    confirmable('remove', ids, function (sub) {
-      return sub.destroy();
-    })
+      .then(confirmable('remove', function (s) {
+        return s.destroy();
+      }))
 
-    .tap(function (s) {
-      console.log('%d subscription(s) removed.', s.length);
-    })
+      .tap(num('removed'))
 
-    .finally(shutdown);
+      .catch(backtrace('Failed to remove subscription(s)'))
+
+      .finally(shutdown);
   });
 
 program
@@ -169,16 +119,18 @@ program
   .description('Reset the given subscription(s)')
 
   .action(function reset(ids) {
+    Subscription
+      .find(ids)
 
-    confirmable('reset', ids, function (sub) {
-      return sub.reset().save();
-    })
+      .then(confirmable('reset', function (s) {
+        return s.reset().save();
+      }))
 
-    .tap(function (subs) {
-      console.log('%d subscription(s) reset.', subs.length);
-    })
+      .tap(num('reset'))
 
-    .finally(shutdown);
+      .catch(backtrace('Failed to reset subscription(s)'))
+
+      .finally(shutdown);
   });
 
 program
@@ -218,3 +170,57 @@ program
 
 program.parse(process.argv);
 
+
+// --- Helper Functions ---
+
+function shutdown() {
+  Subscription.db.reset();
+  process.stdin.destroy();
+}
+
+function num(action) {
+  return function (s) {
+    console.log('%d subscription(s) %s.', s.length, action);
+  };
+}
+
+function backtrace(message) {
+  return function (e) {
+    console.error([message, e.message].join(': '));
+    console.error(e.stack);
+  };
+}
+
+function plugin(string, list) {
+  return list.concat(string.split(','));
+}
+
+function confirm(question) {
+  return new B(function (resolve, reject) {
+    cp.confirm(question + ' Proceed? ')(function (e, ok) {
+      if (e) return reject(e);
+      resolve(ok);
+    });
+  });
+}
+
+function confirmable(method, action) {
+  return function (ss) {
+    var question =
+      [capitalize(method), ss.length, 'subscription(s).'].join(' ');
+
+    return confirm(question)
+      .then(function (ok) {
+        return B.each(ok ? ss : [], action);
+      });
+  };
+}
+
+function redis(input) {
+  var cfg = input.split(':');
+
+  if (cfg[0]) arkivo.config.redis.host = cfg[0];
+  if (cfg[1]) arkivo.config.redis.port = cfg[1];
+
+  return cfg;
+}

cd890a2ddc76132aa5e3d21cef051f64b46c5897
Sylvester Keil <sylvester@keil.or.at>
2014-09-25 13:00:23 +0200

    add logger plugin tests

--- test/plugins/logger.js
+++ test/plugins/logger.js
@@ -1,13 +1,18 @@
-var chai = require('chai');
+var chai   = require('chai');
+var sinon  = require('sinon');
 var expect = chai.expect;
 
+chai.use(require('sinon-chai'));
+
 //chai.use(require('chai-as-promised'));
-//chai.use(require('sinon-chai'));
 
 //var B = require('bluebird');
 
 var plugins = require('../../lib/plugins');
-var logger = require('../../lib/plugins/logger');
+var logger  = require('../../lib/plugins/logger');
+
+var Synchronization = require('../../lib/sync').Synchronization;
+var Subscription    = require('../../lib/subscription');
 
 describe('Plugin "logger"', function () {
 
@@ -16,6 +21,51 @@ describe('Plugin "logger"', function () {
 
   it('is available', function () {
     expect(plugins.available.logger).to.be.an('object');
+  });
+
+  it('can be used', function () {
+    expect(plugins.use('logger')).to.be.instanceof(plugins.Plugin);
+  });
+
+  describe('#process', function () {
+    var sync;
+
+    beforeEach(function () {
+      sync = new Synchronization(new Subscription({
+        url: '/users/42/items'
+      }));
+
+      sinon.stub(console, 'log');
+    });
+
+    afterEach(function () { console.log.restore(); });
+
+    it('prints not modified sync data to console.log by default',
+      function (done) {
+        expect(console.log).to.not.have.been.called;
+
+        expect(sync.modified).to.be.false;
+
+        plugins.use('logger').process(sync, function () {
+          expect(console.log).to.have.been.called;
+          done();
+        });
+      });
+
+    it('prints modified sync data to console.log by default', function (done) {
+      expect(console.log).to.not.have.been.called;
+
+      sync.version = 7;
+      sync.updated.push('foo');
+      sync.items.foo = { data: {} };
+
+      expect(sync.modified).to.be.true;
+
+      plugins.use('logger').process(sync, function () {
+        expect(console.log).to.have.been.called;
+        done();
+      });
+    });
   });
 });
 

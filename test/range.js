d22f95b28fe0d2967633d21b9c604c5c5744f7b4
Sylvester Keil <sylvester@keil.or.at>
2014-10-02 19:14:34 +0200

    fix range limit computation

--- test/range.js
+++ test/range.js
@@ -17,13 +17,27 @@ describe('Range', function () {
       expect(range.finite).to.be.false;
     });
 
+    it('is valid', function () {
+      expect(range.valid).to.be.true;
+    });
+
     it('starts at 0 and has no limit', function () {
       expect(range.start).to.equal(0);
       expect(range.limit).to.be.undefined;
+
+      expect(range.params).to.have.keys(['start']);
     });
   });
 
   describe('when infinite', function () {
+    it('works with to/from input', function () {
+      range = new Range(1, 3);
+
+      expect(range.array).to.eql([1, 3]);
+      expect(range.start).to.eql(1);
+
+      expect(range.limit).to.eql(2);
+    });
   });
 
   describe('when finite', function () {

6e7078ffa649030210a80632570000c3374b986d
Sylvester Keil <sylvester@keil.or.at>
2014-11-17 17:59:16 +0100

    add more rest test

--- test/plugins/rest.js
+++ test/plugins/rest.js
@@ -1,4 +1,5 @@
 var chai   = require('chai');
+var nock   = require('nock');
 //var sinon  = require('sinon');
 //var B      = require('bluebird');
 
@@ -36,15 +37,59 @@ describe('Plugin "rest"', function () {
   });
 
   describe('#process', function () {
+    var HOST = 'http://example.com';
     var sync;
+    var plugin;
 
     beforeEach(function () {
       sync = new Session(new Subscription({
         url: '/users/42/items'
       }));
+
+      sync.version = 11;
+      sync.items.foo = {
+        key: 'foo',
+        data: {}
+      };
+
+      plugin = plugins.use('rest', {
+        create: HOST + '/api/items',
+        update: HOST + '/api/items/:key',
+        delete: HOST + '/api/items/:key'
+      });
     });
 
-    afterEach(function () {});
+    it('calls create path for all created items', function () {
+      nock(HOST)
+        .post('/api/items', sync.items.foo)
+        .reply(200);
 
+      sync.created.push('foo');
+
+      return expect(plugin.process(sync))
+        .to.eventually.be.fulfilled;
+    });
+
+    it('calls update path for all updated items', function () {
+      nock(HOST)
+        .put('/api/items/foo', sync.items.foo)
+        .reply(200);
+
+      sync.updated.push('foo');
+
+      return expect(plugin.process(sync))
+        .to.eventually.be.fulfilled;
+    });
+
+    it('calls delete path for all deleted ids', function () {
+      nock(HOST)
+        .delete('/api/items/foo')
+        .reply(201);
+
+      sync.deleted.push('foo');
+
+      return plugin.process(sync);
+        //.to.eventually.be.fulfilled;
+    });
   });
 });

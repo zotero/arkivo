5c4da21aa562169ea2bf0f9034af87f7665af322
Sylvester Keil <sylvester@keil.or.at>
2014-11-17 16:51:52 +0100

    rename plugin restful -> rest

--- test/plugins/rest.js
+++ test/plugins/rest.js
@@ -8,31 +8,31 @@ chai.use(require('sinon-chai'));
 chai.use(require('chai-as-promised'));
 
 var plugins      = require('../../lib/plugins');
-var restful      = require('../../lib/plugins/restful');
+var rest         = require('../../lib/plugins/rest');
 
 var Session      = require('../../lib/sync').Session;
 var Subscription = require('../../lib/subscription');
 
 
-describe('Plugin "restful"', function () {
+describe('Plugin "rest"', function () {
 
-  before(function () { plugins.add(restful); });
+  before(function () { plugins.add(rest); });
   after(function () { plugins.reset(); });
 
   it('is available', function () {
-    expect(plugins.available.restful).to.be.an('object');
+    expect(plugins.available.rest).to.be.an('object');
   });
 
   it('can be used', function () {
-    expect(plugins.use('restful')).to.be.instanceof(plugins.Plugin);
+    expect(plugins.use('rest')).to.be.instanceof(plugins.Plugin);
   });
 
   it('is configurable', function () {
-    expect(plugins.use('restful').configurable).to.be.true;
+    expect(plugins.use('rest').configurable).to.be.true;
   });
 
   it('has a summary', function () {
-    expect(plugins.use('restful')).to.have.property('summary');
+    expect(plugins.use('rest')).to.have.property('summary');
   });
 
   describe('#process', function () {

74f42b2833930ee1122353abf8ccac6d672827d3
Sylvester Keil <sylvester@keil.or.at>
2015-03-16 19:14:01 +0100

    replace config module

--- test/config.js
+++ test/config.js
@@ -6,26 +6,26 @@ var config = require('../lib/config');
 describe('config', function () {
 
   it('has readers for each module section', function () {
-    expect(config.proxy)
+    expect(config)
       .to.have.property('q')
       .and.have.property('prefix', 'q');
   });
 
   it('contains the configuration options', function () {
-    expect(config.proxy.subscription).to.have.property('prefix', 's');
-    expect(config.proxy.q).to.have.property('prefix', 'q');
+    expect(config.subscription).to.have.property('prefix', 's');
+    expect(config.q).to.have.property('prefix', 'q');
 
-    expect(config.proxy.redis).to.have.property('host', '127.0.0.1');
-    expect(config.proxy.redis).to.have.property('port', 6379);
+    expect(config.redis).to.have.property('host', '127.0.0.1');
+    expect(config.redis).to.have.property('port', 6379);
   });
 
   describe('#update', function () {
     it('updates existing configuration options', function () {
-      var original = config.proxy.redis;
+      var original = config.redis;
 
-      config.proxy.update({ redis: { bar: 'baz' } });
+      config.update({ redis: { bar: 'baz' } });
 
-      expect(config.proxy.redis).to.have.property('bar', 'baz');
+      expect(config.redis).to.have.property('bar', 'baz');
       expect(original).to.have.property('bar', 'baz');
     });
   });
